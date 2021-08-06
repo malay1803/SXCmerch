3d7c51f3f4d8511625fa8708fc5be63474f9cf86
aayushjagwani <ayushja678@gmail.com>
2021-08-06 13:02:21 +0530

    changesss

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const User = require('./models/user');
 const { urlencoded } = require('express');
 const methodOverride = require('method-override');
 const bcrypt = require('bcrypt');
-// const session = require('express-session');
-// const flash = require('connect-flash');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 mongoose.connect('mongodb://localhost:27017/sxcdatabase', {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => {
@@ -24,15 +24,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
-// app.use(session({ secret: 'aayushjagwani' , resave: false , saveUninitialized: true}));
-// app.use(flash());
+app.use(session({ secret: 'aayushjagwani' , resave: false , saveUninitialized: true}));
+app.use(flash());
 
 
 app.get('/', (req,res) => {
     res.render('home');
 })
 
-app.post('/',async (req,res)=>{
+app.post('/signup',async (req,res)=>{
     const {Name,Email,Password,cPassword}=req.body;
 
     if(Password == cPassword ){
@@ -46,6 +46,29 @@ app.post('/',async (req,res)=>{
     // res.send("Password donot match");
 })
 
+app.post('/login', async (req,res) =>{
+    const {Email1,Password1}= req.body;
+    const user = await User.findOne({Email: Email1});
+    if(!user)
+    {
+        req.flash('error', 'Incorrect Credentials, Try Again!');
+        res.redirect('/login');
+        
+    }
+    const validPwd = await bcrypt.compare(Password1, user.Password);
+    if(validPwd)
+    {
+        console.log('logged in!!');
+        req.session.user_id = user._id;
+        res.redirect('/merchandise');
+    }
+    else
+    {
+        reqflash('error', 'Incorrect Credentials, Try Again!');
+        res.redirect('/login');
+    }
+} )
+
 app.get('/merchandise',(req,res) => {
     res.render('merchandise');
 })
@@ -55,9 +78,7 @@ app.get('/about',(req,res) => {
 app.get('/contact',(req,res) => {
     res.render('contact');
 })
-app.get('/login',(req,res) => {
-    res.render('contact');
-})
+
 app.listen(3000, () =>{
     console.log("Listening on port 3000")
 })
