9c6fe82db8162dff21e34d179b5bf7458f5694d3
malay1803 <18acharyamalay@gmail.com>
2021-08-09 18:46:56 +0530

    added merchandise cards

--- seeds/data.js
+++ seeds/data.js
@@ -1,9 +1,9 @@
 module.exports = [
-    {
-        pName: 'Malhar Tshirt(White)',
-        pPrice: 499,
-        pImage: '/image/merch/hangTshirtWhite.jpg',
-        pDescription: 'White Cotton Tshirt with Malhar 2021 logo on the front.',
-        pCategory: 'tshirt'
-    }
-]
+  {
+    pName: "Malhar Tshirt(White)",
+    pPrice: 499,
+    pImage: "/image/merch/hangTshirtWhite.jpg",
+    pDescription: "White Cotton Tshirt with Malhar 2021 logo on the front.",
+    pCategory: "tshirt",
+  },
+];
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,29 +1,32 @@
-const mongoose = require('mongoose');
-const Product = require('../models/merchandise');
-const item = require('./data');
+const mongoose = require("mongoose");
+const Product = require("../models/merchandise");
+const item = require("./data");
 
-mongoose.connect('mongodb://localhost:27017/sxcdatabase', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose
+  .connect("mongodb://localhost:27017/sxcdatabase", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-      console.log("Mongo Connection Open!!");
+    console.log("Mongo Connection Open!!");
   })
-  .catch(err => {
-      console.log("ERROR");
-      console.log(err);
-  })
+  .catch((err) => {
+    console.log("ERROR");
+    console.log(err);
+  });
 
 const seedDB = async () => {
-    await Product.deleteMany({});
-    for(let i=0;i<item.length;i++) {
-        const store = new Product({
-            pName: item[i].pName,
-            pPrice: item[i].pPrice,
-            pImage: item[i].pImage,
-            pDescription: item[i].pDescription,
-            pCategory: item[i].pCategory
-        });
-        await store.save();
-    }
-    
-}
+  await Product.deleteMany({});
+  for (let i = 0; i < item.length; i++) {
+    const store = new Product({
+      pName: item[i].pName,
+      pPrice: item[i].pPrice,
+      pImage: item[i].pImage,
+      pDescription: item[i].pDescription,
+      pCategory: item[i].pCategory,
+    });
+    await store.save();
+  }
+};
 
 seedDB();
