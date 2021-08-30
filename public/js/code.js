4d229fd068f00de5716b2236715df8bf5bf66cd2
Abhishek <abhishek.rathore@xaviers.edu.in>
2021-08-30 11:35:02 +0530

    time

--- public/js/code.js
+++ public/js/code.js
@@ -93,4 +93,16 @@ function checkphone(inp){
         }
 }
 
-
+function display_ct5() {
+    var x = new Date()
+    var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
+    
+    var x1 =  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds() + " " + ampm;
+    document.getElementById('timeimp').innerHTML = x1;
+    display_c5();
+}
+function display_c5(){
+    var refresh=1000; // Refresh rate in milli seconds
+    mytime=setTimeout('display_ct5()',refresh)
+}
+display_c5()
